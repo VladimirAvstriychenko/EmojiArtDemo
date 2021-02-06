5da7f5a2ca1135fd47d986a6e730fe2ddd98f77f
Vladimir Avstirychenko <liquid-05@mail.ru>
2021-02-06 18:56:33 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArt.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Владимир on 05.02.2021.
+//
+
+import Foundation
+
+struct EmojiArt: Codable{
+    
+    var url: URL
+    var emojis = [EmojiInfo]()
+    var json:Data? {
+        return try? JSONEncoder().encode(self)
+    }
+    
+    struct EmojiInfo: Codable{
+        let x: Int
+        let y: Int
+        let text: String
+        let size: Int
+    }
+    
+    init(url: URL, emojis: [EmojiInfo]) {
+        self.url = url
+        self.emojis = emojis
+    }
+    
+    init?(json: Data){
+        if let newValue = try? JSONDecoder().decode(EmojiArt.self, from: json) {
+            self = newValue
+        } else {
+            return nil
+        }
+    }
+}
---
+++ EmojiArt/TextFieldCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TextFieldCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Владимир on 05.02.2021.
+//
+
+import UIKit
+
+class TextFieldCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.delegate = self
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    var resignationHandler: (() -> Void)?
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        resignationHandler?()
+    }
+    
+}
